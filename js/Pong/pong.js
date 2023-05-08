0f2e67e3042467b900caf7f87e37b084276bf7ad
Acumiie <vivwongjiahui@gmail.com>
2023-05-08 22:25:00 +0800

    1.2.2

--- js/Pong/pong.js
+++ js/Pong/pong.js
@@ -27,10 +27,37 @@ ww....ww
 	ball.bounciness = 1; // full bounciness
 	ball.friction = 0; // no friction
 
+	ball.direction = 45;
+	ball.speed = 5;
+
 	// TODO: create paddle sprites
+	paddleL = new Sprite();
+	paddleL.image = imgPaddle;
+	paddleL.x = 20;
+	paddleL.y = 98;
+	paddleL.collider = 'k';
+	paddleR = new Sprite();
+	paddleR.image = imgPaddle;
+	paddleR.x = 235;
+	paddleR.y = 100;
+	paddleR.collider = 'k';
+
+	wallT = new Sprite();
+	wallT.w = 260;
+	wallT.h = 1;
+	wallT.y = 0;
+	wallT.x = 100;
+	wallT.collider = 's';
+	wallB = new Sprite();
+	wallB.w = 260;
+	wallB.h = 1;
+	wallB.y = 100;
+	wallB.x = 100;
+	wallB.collider = 's';
 }
 
 // code in the draw function gets run 60 times per second
 function draw() {
+	background('b');
 	// TODO: move the paddles
 }
